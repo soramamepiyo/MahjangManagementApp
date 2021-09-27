96c93f3920489b13987c59b09809077a5e700dd7
Sora Nakanishi <sora10201020@icloud.com>
2021-09-27 23:51:05 +0900

    Initial Commit

---
+++ MahjangManagementApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MahjangManagementApp
+//
+//  Created by 中西空 on 2021/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
