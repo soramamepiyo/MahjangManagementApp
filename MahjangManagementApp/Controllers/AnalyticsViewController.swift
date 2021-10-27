38af4ac375efba623c4c7d2f359d918f079872f2
Sora Nakanishi <sora10201020@icloud.com>
2021-10-27 10:34:27 +0900

    分析画面の期間別分析機能の追加

--- MahjangManagementApp/Controllers/AnalyticsViewController.swift
+++ MahjangManagementApp/Controllers/AnalyticsViewController.swift
@@ -39,12 +39,12 @@ class AnalyticsViewController: UIViewController, UIPickerViewDelegate, UIPickerV
     @IBOutlet weak var pointOf4Label: UILabel!
     @IBOutlet weak var pointOf3Label: UILabel!
     
-//    let termList = [
-//        "全期間",
-//        "1月", "2月", "3月", "4月", "5月", "6月",
-//        "7月", "8月", "9月", "10月", "11月", "12月"]
+    let termList = [
+        "全期間",
+        "2021年10月", "2021年11月", "2021年12月",
+        "2022年1月", "2022年2月", "2022年3月", "2022年4月", "2022年5月", "2022年6月"]
     
-    let termList = ["全期間"]
+//    let termList = ["全期間"]
     
     var selectedTerm: String = ""
     
@@ -117,9 +117,25 @@ class AnalyticsViewController: UIViewController, UIPickerViewDelegate, UIPickerV
     func pickerView(_ pickerView: UIPickerView,didSelectRow row: Int, inComponent component: Int) {
         
         selectedTerm = termList[row]
-        
-        print("\(selectedTerm)が選択されました。")
-        
+                
+        if row == 0 {
+            termIsAllShowResults()
+        } else if row <= 9 {
+            
+            var month = row + 9
+            var year = 2021
+            
+            if month > 12 {
+                year += 1
+                month -= 12
+            }
+            
+            print("\(year)年\(month)月のデータを探索します")
+            termIsMonthShowResults(year: year, month: month)
+        } else {
+            print("探索エラーです。")
+        }
+            
     }
     
     private func termIsAllShowResults() {
@@ -241,6 +257,149 @@ class AnalyticsViewController: UIViewController, UIPickerViewDelegate, UIPickerV
             
             let totalRankOf4: Float = Float(firstOf4 * 1 + secondOf4 * 2 + thirdOf4 * 3 + forthOf4 * 4)
             let avarageOf4 = totalRankOf4 / totalOf4
+//            print(avarageOf4)
+            
+            let totalRankOf3: Float = Float(firstOf3 * 1 + secondOf3 * 2 + thirdOf3 * 3)
+            let avarageOf3 = totalRankOf3 / totalOf3
+            
+            pointOf4Label.text = "\(String(format: "%.1f", pointOf4))P"
+            pointOf3Label.text = "\(String(format: "%.1f", pointOf3))P"
+
+            showAnalytics(totalOf4: Int(totalOf4), totalOf3: Int(totalOf3), avarageOf4: avarageOf4, avarageOf3: avarageOf3)
+        }
+    }
+    
+    
+    private func termIsMonthShowResults(year: Int, month: Int) {
+        
+        labelReset()
+        
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        var firstOf4 = 0
+        var secondOf4 = 0
+        var thirdOf4 = 0
+        var forthOf4 = 0
+        
+        var firstOf3 = 0
+        var secondOf3 = 0
+        var thirdOf3 = 0
+        
+        var pointOf4: Float = 0
+        var pointOf3: Float = 0
+        
+        Firestore.firestore().collection("mahjang").document("results").collection(uid).order(by: "date").getDocuments { [self] (snapShots, err) in
+        
+            if let err = err {
+                print("Firestoreからのデータの取得に失敗しました。\(err)")
+                
+                HUD.hide { (_) in
+                    HUD.flash(.error, delay: 1)
+                }
+                
+                return
+            }
+            
+            
+            snapShots?.documents.forEach({ (snapShot) in
+                let dic = snapShot.data()
+                let result = Result.init(dic: dic)
+                
+                let term = getRuleIdentifierString(date: result.date.dateValue())
+                                                
+                let targetTerm = String(year) + String(month)
+                
+                if term == targetTerm {
+                    switch result.mode {
+                    
+                    case "4":
+                        
+                        switch result.ranking {
+                        
+                        case 1:
+                            firstOf4 = firstOf4 + 1
+                                            
+                        case 2:
+                            secondOf4 = secondOf4 + 1
+                            
+                        case 3:
+                            thirdOf4 = thirdOf4 + 1
+                            
+                        case 4:
+                            forthOf4 = forthOf4 + 1
+                            
+                        default:
+                            break
+                        }
+                        
+                        pointOf4 = pointOf4 + result.point
+                        
+                    case "3":
+                        switch result.ranking {
+                        
+                        case 1:
+                            firstOf3 = firstOf3 + 1
+                            firstOf3Label.text = String(firstOf3)
+                        
+                        case 2:
+                            secondOf3 = secondOf3 + 1
+                            secondOf3Label.text = String(secondOf3)
+                            
+                        case 3:
+                            thirdOf3 = thirdOf3 + 1
+                            thirdOf3Label.text = String(thirdOf3)
+                            
+                        default:
+                            break
+                        }
+                        
+                        pointOf3 = pointOf3 + result.point
+                        
+                    default:
+                        break
+                        
+                    }
+                }
+            
+                
+            })
+            
+            let totalOf4: Float = Float(firstOf4 + secondOf4 + thirdOf4 + forthOf4)
+            let totalOf3: Float = Float(firstOf3 + secondOf3 + thirdOf3)
+            
+            let firstOf4Rate: String = String(format: "%.1f", Float(firstOf4 * 100) / totalOf4)
+            let secondOf4Rate: String = String(format: "%.1f", Float(secondOf4 * 100) / totalOf4)
+            let thirdOf4Rate: String = String(format: "%.1f", Float(thirdOf4 * 100) / totalOf4)
+            let forthOf4Rate: String = String(format: "%.1f", Float(forthOf4 * 100) / totalOf4)
+            
+            let firstOf3Rate: String = String(format: "%.1f", Float(firstOf3 * 100) / totalOf3)
+            let secondOf3Rate: String = String(format: "%.1f", Float(secondOf3 * 100) / totalOf3)
+            let thirdOf3Rate: String = String(format: "%.1f", Float(thirdOf3 * 100) / totalOf3)
+            
+            if totalOf4 == 0 {
+                firstOf4Label.text = "0回 (0.0)%"
+                secondOf4Label.text = "0回 (0.0)%"
+                thirdOf4Label.text = "0回 (0.0)%"
+                forthOf4Label.text = "0回 (0.0)%"
+            } else {
+                firstOf4Label.text = "\(firstOf4)回 (\(firstOf4Rate))%"
+                secondOf4Label.text = "\(secondOf4)回 (\(secondOf4Rate))%"
+                thirdOf4Label.text = "\(thirdOf4)回 (\(thirdOf4Rate))%"
+                forthOf4Label.text = "\(forthOf4)回 (\(forthOf4Rate))%"
+            }
+            
+            if totalOf3 == 0 {
+                firstOf3Label.text = "0回 (0.0)%"
+                secondOf3Label.text = "0回 (0.0)%"
+                thirdOf3Label.text = "0回 (0.0)%"
+            } else {
+                firstOf3Label.text = "\(firstOf3)回 (\(firstOf3Rate))%"
+                secondOf3Label.text = "\(secondOf3)回 (\(secondOf3Rate))%"
+                thirdOf3Label.text = "\(thirdOf3)回 (\(thirdOf3Rate))%"
+            }
+            
+            let totalRankOf4: Float = Float(firstOf4 * 1 + secondOf4 * 2 + thirdOf4 * 3 + forthOf4 * 4)
+            let avarageOf4 = totalRankOf4 / totalOf4
             print(avarageOf4)
             
             let totalRankOf3: Float = Float(firstOf3 * 1 + secondOf3 * 2 + thirdOf3 * 3)
@@ -251,6 +410,42 @@ class AnalyticsViewController: UIViewController, UIPickerViewDelegate, UIPickerV
 
             showAnalytics(totalOf4: Int(totalOf4), totalOf3: Int(totalOf3), avarageOf4: avarageOf4, avarageOf3: avarageOf3)
         }
+    }
+    
+    private func labelReset() {
+        firstOf4Label.text = "0"
+        secondOf4Label.text = "0"
+        thirdOf4Label.text = "0"
+        forthOf4Label.text = "0"
+        firstOf3Label.text = "0"
+        secondOf3Label.text = "0"
+        thirdOf3Label.text = "0"
+        totalOf4Label.text = "0"
+        totalOf3Label.text = "0"
+        avarageRankOf4Label.text = "-----"
+        avarageRankOf3Label.text = "-----"
+        pointOf4Label.text = "0.0P"
+        pointOf3Label.text = "0.0P"
+    }
+    
+    //時刻を見やすく修正する関数
+    private func getRuleIdentifierString(date: Date) -> String {
+    
+        let dateFormatter = DateFormatter()
+        
+        // フォーマット設定
+        dateFormatter.dateFormat = "yyyyMM"
+
+        // ロケール設定（端末の暦設定に引きづられないようにする）
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+
+        // タイムゾーン設定（端末設定によらず固定にしたい場合）
+        dateFormatter.timeZone = TimeZone(identifier: "Asia/Tokyo")
+
+        // 変換
+        let str = dateFormatter.string(from: date)
+        
+        return str
     }
     
     private func showAnalytics(totalOf4: Int, totalOf3: Int, avarageOf4: Float, avarageOf3: Float) {
